86145152f750273cb6428d331351e937f7722f33
Jason McCallister <themccallister@gmail.com>
2017-08-19 21:12:15 -0400

    Create an empty package for helpers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,11 +1,8 @@
 package handlers
 
-import (
-	"net/http"
+// HELPERS
 
-	"github.com/themccallister/go-https-demo/response"
-)
-
-func Index(w http.ResponseWriter, req *http.Request) {
-	response.JSON(w, []byte("message loaded ok"), http.StatusOK)
-}
+// This contains all of the helper methods, variables and constants
+// used throughout the package. Before attempting to create a new
+// function to share logic in this package, you should check for
+// pre-existing code here, if it does not exist you can add it here.
