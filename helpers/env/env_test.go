dc89205728259e7fcaf5063c64ead6370e14d87b
Jason McCallister <themccallister@gmail.com>
2017-08-19 22:06:15 -0400

    Update tests after package moves

--- helpers/env/env_test.go
+++ helpers/env/env_test.go
@@ -4,13 +4,13 @@ import (
 	"os"
 	"testing"
 
-	"github.com/themccallister/go-https-demo/helpers"
+	"github.com/themccallister/go-https-demo/helpers/env"
 )
 
 func TestGetBoolOr(t *testing.T) {
 	os.Setenv("TEST_ENV", "true")
 
-	e := helpers.GetBoolOr("TEST_ENV", false)
+	e := env.GetBoolOr("TEST_ENV", false)
 
 	if e != true {
 		t.Fatalf("expected the env var to be true got %v instead", e)
@@ -18,7 +18,7 @@ func TestGetBoolOr(t *testing.T) {
 
 	os.Setenv("TEST_ENV", "")
 
-	ne := helpers.GetBoolOr("TEST_ENV", false)
+	ne := env.GetBoolOr("TEST_ENV", false)
 
 	if ne != false {
 		t.Fatalf("expected the env var to be false, got %v instead", ne)
@@ -27,16 +27,16 @@ func TestGetBoolOr(t *testing.T) {
 	// string must be true or false
 	os.Setenv("TEST_ENV", "this is not true")
 
-	nt := helpers.GetBoolOr("TEST_ENV", false)
+	nt := env.GetBoolOr("TEST_ENV", false)
 	if nt != false {
 		t.Fatalf("expected the env var to be false, got %v instead", nt)
 	}
 }
 
-func TestGetStringOr(t *testing.T) {
+func TestGetOr(t *testing.T) {
 	os.Setenv("TEST_ENV", "testing")
 
-	e := helpers.GetStringOr("TEST_ENV", "not testing")
+	e := env.GetOr("TEST_ENV", "not testing")
 
 	if e != "testing" {
 		t.Fatalf("expected the string to be testing, got %v instead", e)
@@ -44,7 +44,7 @@ func TestGetStringOr(t *testing.T) {
 
 	os.Setenv("TEST_ENV", "")
 
-	ne := helpers.GetStringOr("TEST_ENV", "not testing")
+	ne := env.GetOr("TEST_ENV", "not testing")
 
 	if ne != "not testing" {
 		t.Fatalf("expected the string to be not testing, got %v instead", ne)
